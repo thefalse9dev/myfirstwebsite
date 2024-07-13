471a2ed745764674a15a94f674bb8fe0fd3e7ab9
sai sudeep <saisudeep@sais-MacBook-Air.local>
2024-07-13 08:56:54 +0530

    fixed the email extraction default bug

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ let user = document.getElementById("email");
 console.log(user)
 console.log(m)
 function btnClick(){
-    var nameMatch = user.value.match(/^([^@]*)@/)[1]
+    var nameMatch = user.value.match(/^([^@]*)@/)
+    nameMatch = nameMatch ? nameMatch[1] : "user"
     m.innerHTML='Logging '+'<span id="span-1">'+ nameMatch+'</span>'+' in, please wait';
     setTimeout(function(){
         m.innerText=""
